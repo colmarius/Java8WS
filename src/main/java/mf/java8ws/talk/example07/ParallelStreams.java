af021dacd92833c0c9df4bbbfb7da909ad6847a9
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-11-26 11:25:25 +0100

    Example 7: measure of sequential and parallel streams
    Fork/Join framework behind this.

--- src/main/java/mf/java8ws/talk/example07/ParallelStreams.java
+++ src/main/java/mf/java8ws/talk/example07/ParallelStreams.java
@@ -1,13 +1,33 @@
 package mf.java8ws.talk.example07;
 
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class ParallelStreams {
 
     public static long sequentialSum(long n) {
-        return 0L;
+        return Stream.iterate(1L, i -> i + 1) // this is an infinite stream
+                    .limit(n)
+                    .reduce(Long::sum).get(); // returns an Optional need to call .get()
     }
 
     public static long parallelSum(long n) {
-        return 0L;
+        return Stream.iterate(1L, i -> i + 1)
+                .parallel() // Laziness plays against us. Fork/Join framework is taking a lot of time to catchup.
+                .limit(n)
+                .reduce(Long::sum).get();
     }
 
+    public static long rangedSum(long n) {
+        return LongStream.rangeClosed(1, n)
+                .limit(n)
+                .reduce(Long::sum).getAsLong();
+    }
+
+    public static long parallelRangedSum(long n) {
+        return LongStream.rangeClosed(1, n)
+                .parallel() // Fork/Join framework works better in this case.
+                .limit(n)
+                .reduce(Long::sum).getAsLong();
+    }
 }
--- src/main/java/mf/java8ws/talk/example07/ParallelStreamsHarness.java
+++ src/main/java/mf/java8ws/talk/example07/ParallelStreamsHarness.java
@@ -10,8 +10,8 @@ public class ParallelStreamsHarness {
     public static void main(String[] args) {
         System.out.println("Sequential Sum done in: " + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + " msecs");
         System.out.println("Parallel Sum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + " msecs");
-        //System.out.println("Sequential ranged sum done in: " + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + " msecs" );
-        //System.out.println("Parallel ranged sum done in: " + measurePerf(ParallelStreams::parallelRangedSum, 10_000_000L) + " msecs" );
+        System.out.println("Sequential ranged sum done in: " + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + " msecs" );
+        System.out.println("Parallel ranged sum done in: " + measurePerf(ParallelStreams::parallelRangedSum, 10_000_000L) + " msecs" );
     }
 
     public static <T, R> long measurePerf(Function<T, R> f, T input) {
