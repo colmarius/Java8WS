6762bf9fe5b9526636005a8daececeab40debb56
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-11-26 12:39:10 +0100

    Example 10: using optionals in Java 8

--- src/main/java/mf/java8ws/talk/example10/OptionalTest.java
+++ src/main/java/mf/java8ws/talk/example10/OptionalTest.java
@@ -19,9 +19,38 @@ public class OptionalTest {
         assertEquals(0, readDuration(param, "b"));
         assertEquals(0, readDuration(param, "c"));
         assertEquals(0, readDuration(param, "d"));
+
+        assertEquals(5, readDurationImproved(param, "a"));
+        assertEquals(0, readDurationImproved(param, "b"));
+        assertEquals(0, readDurationImproved(param, "c"));
+        assertEquals(0, readDurationImproved(param, "d"));
     }
 
     public int readDuration(Map<String, String> params, String name) {
-        return 0;
+        String value = params.get(name);
+        if (value == null) {
+            return 0;
+        }
+        try {
+            int i = Integer.parseInt(value);
+            return (i > 0) ? i : 0;
+        } catch(NumberFormatException nfe) {
+            return 0;
+        }
+    }
+
+    public int readDurationImproved(Map<String, String> params, String name) {
+        return Optional.ofNullable(params.get(name))
+                .flatMap(OptionalTest::string2int)
+                .filter(i -> i > 0)
+                        .orElse(0);
+    }
+
+    public static Optional<Integer> string2int(String s) {
+        try {
+            return Optional.of(Integer.parseInt(s));
+        } catch(NumberFormatException nfe) {
+            return Optional.empty();
+        }
     }
 }
